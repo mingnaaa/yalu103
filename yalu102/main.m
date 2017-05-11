b05ecfba216efffceaf663d0b52f7eab06a4f446
mingnaaa <kanitpong@iMinGX.local>
2017-05-12 04:18:27 +0700

    Revert "jailbreak files (10.2 for now)"
    This reverts commit a9ce9ee835ef19ed674485353c43507078878c4a.
    
    # Conflicts:
    #	yalu103.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    #
    yalu103.xcodeproj/xcuserdata/noahterbest.xcuserdatad/xcschemes/xcschemem
    anagement.plist
    #
    yalu103.xcodeproj/xcuserdata/noahterbest.xcuserdatad/xcschemes/yalu103.x
    cscheme
    #	yalu103/.DS_Store
    #	yalu103/AppDelegate.m
    #	yalu103/Assets.xcassets/AppIcon.appiconset/Contents.json
    #	yalu103/Assets.xcassets/Contents.json
    #	yalu103/Assets.xcassets/Icon-iPadPro.imageset/Contents.json
    #	yalu103/Info.plist
    #	yalu103/main.m

---
+++ yalu102/main.m
@@ -0,0 +1,21 @@
+//
+<<<<<<< HEAD:yalu103/main.m
+//  yalu103Tests.m
+//  yalu103Tests
+=======
+//  main.m
+//  yalu102
+>>>>>>> parent of 55da7b9... application rewrite:yalu102/main.m
+//
+//  Created by qwertyoruiop on 05/01/2017.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
